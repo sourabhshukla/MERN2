c724f1b8d42cc78d94752ce0f31f163743b47b90
sourabhshukla <sourabhshukla9893502147@gmail.com>
2022-07-31 16:58:35 +0530

    Implemented Lazy Loading!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from "./Components/Header/Header";
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, Suspense, lazy} from "react";
 import Home from "./Pages/Home/Home";
 import {
     Routes,
     Route
 } from "react-router-dom";
-import AllProducts from "./Pages/AllProducts/AllProducts";
-import LoginSignUp from "./Pages/LoginSignUp/LoginSignUp";
-import ProductDetails from "./Pages/ProductDetails/ProductDetails";
-import Profile from "./Pages/Profile/Profile";
-import UpdateProfile from "./Pages/UpdateProfile/UpdateProfile";
-import UpdatePassword from "./Pages/UpdatePassword/UpdatePassword";
-import ForgotPassword from "./Pages/ForgotPassword/ForgotPassword";
-import ResetPassword from "./Pages/ResetPassword/ResetPassword";
-import Cart from "./Pages/Cart/Cart";
-import Shipping from "./Pages/Shipping/Shipping";
-import ConfirmOrder from "./Pages/ConfirmOrder/ConfirmOrder";
-import Payment from "./Pages/Payment/Payment";
-import OrderSuccess from "./Pages/OrderSuccess/OrderSuccess";
-import MyOrders from "./Pages/MyOrders/MyOrders";
-import OrderDetails from "./Pages/OrderDetails/OrderDetails";
-import Dashboard from "./Pages/Dashboard/Dashboard";
-import ProductList from "./Pages/ProductList/ProductList";
-import NewProduct from "./Pages/NewProduct/NewProduct";
 import store from "./store";
 import {loadUser} from "./actions/userAction";
 import ProtectedRoute from "./Components/Route/ProtectedRoute";
 import axios from "axios";
 import {Elements} from "@stripe/react-stripe-js";
 import {loadStripe} from "@stripe/stripe-js";
-import UpdateProduct from "./Pages/UpdateProduct/UpdateProduct";
-import OrderList from "./Pages/OrderList/OrderList";
-import ProcessOrder from "./Pages/ProcessOrder/ProcessOrder";
-import UsersList from "./Pages/UsersList/UsersList";
-import UpdateUser from "./Pages/UpdateUser/UpdateUser";
-import ProductReviews from "./Pages/ProductReviews/ProductReviews";
-import NotFound from "./Pages/NotFound/NotFound";
+//import AllProducts from "./Pages/AllProducts/AllProducts";
+const AllProducts = lazy(()=>import('./Pages/AllProducts/AllProducts'));
+// import LoginSignUp from "./Pages/LoginSignUp/LoginSignUp";
+const LoginSignUp = lazy(()=>import('./Pages/LoginSignUp/LoginSignUp'));
+// import ProductDetails from "./Pages/ProductDetails/ProductDetails";
+const ProductDetails = lazy(()=>import('./Pages/ProductDetails/ProductDetails'));
+// import Profile from "./Pages/Profile/Profile";
+const Profile = lazy(()=>import('./Pages/Profile/Profile'));
+// import UpdateProfile from "./Pages/UpdateProfile/UpdateProfile";
+const UpdateProfile = lazy(()=>import('./Pages/UpdateProfile/UpdateProfile'));
+// import UpdatePassword from "./Pages/UpdatePassword/UpdatePassword";
+const UpdatePassword = lazy(()=>import('./Pages/UpdatePassword/UpdatePassword'));
+// import ForgotPassword from "./Pages/ForgotPassword/ForgotPassword";
+const ForgotPassword = lazy(()=>import('./Pages/ForgotPassword/ForgotPassword'));
+// import ResetPassword from "./Pages/ResetPassword/ResetPassword";
+const ResetPassword = lazy(()=>import('./Pages/ResetPassword/ResetPassword'));
+// import Cart from "./Pages/Cart/Cart";
+const Cart = lazy(()=>import('./Pages/Cart/Cart'));
+// import Shipping from "./Pages/Shipping/Shipping";
+const Shipping = lazy(()=>import('./Pages/Shipping/Shipping'));
+// import ConfirmOrder from "./Pages/ConfirmOrder/ConfirmOrder";
+const ConfirmOrder = lazy(()=>import('./Pages/ConfirmOrder/ConfirmOrder'));
+// import Payment from "./Pages/Payment/Payment";
+const Payment = lazy(()=>import('./Pages/Payment/Payment'));
+// import OrderSuccess from "./Pages/OrderSuccess/OrderSuccess";
+const OrderSuccess = lazy(()=>import('./Pages/OrderSuccess/OrderSuccess'));
+// import MyOrders from "./Pages/MyOrders/MyOrders";
+const MyOrders = lazy(()=>import('./Pages/MyOrders/MyOrders'));
+// import OrderDetails from "./Pages/OrderDetails/OrderDetails";
+const OrderDetails = lazy(()=>import('./Pages/OrderDetails/OrderDetails'));
+// import Dashboard from "./Pages/Dashboard/Dashboard";
+const Dashboard = lazy(()=>import('./Pages/Dashboard/Dashboard'));
+// import ProductList from "./Pages/ProductList/ProductList";
+const ProductList = lazy(()=>import('./Pages/ProductList/ProductList'));
+// import NewProduct from "./Pages/NewProduct/NewProduct";
+const NewProduct = lazy(()=>import('./Pages/NewProduct/NewProduct'));
+// import UpdateProduct from "./Pages/UpdateProduct/UpdateProduct";
+const UpdateProduct = lazy(()=>import('./Pages/UpdateProduct/UpdateProduct'));
+// import OrderList from "./Pages/OrderList/OrderList";
+const OrderList = lazy(()=>import('./Pages/OrderList/OrderList'));
+// import ProcessOrder from "./Pages/ProcessOrder/ProcessOrder";
+const ProcessOrder = lazy(()=>import('./Pages/ProcessOrder/ProcessOrder'));
+// import UsersList from "./Pages/UsersList/UsersList";
+const UsersList = lazy(()=>import('./Pages/UsersList/UsersList'));
+// import UpdateUser from "./Pages/UpdateUser/UpdateUser";
+const UpdateUser = lazy(()=>import('./Pages/UpdateUser/UpdateUser'));
+// import ProductReviews from "./Pages/ProductReviews/ProductReviews";
+const ProductReviews = lazy(()=>import('./Pages/ProductReviews/ProductReviews'));
+// import NotFound from "./Pages/NotFound/NotFound";
+const NotFound = lazy(()=>import('./Pages/NotFound/NotFound'));
 
 function App() {
 
@@ -54,9 +79,10 @@ function App() {
     },[]);
   return (
     <div className="App">
-      <>
+      <><Suspense fallback={<div>Please wait...</div>}>
           <Routes>
               <Route path="/" element={<Home/>}/>
+
               <Route path="/product/:id" element={<ProductDetails/>}/>
               <Route path="/products" element={<AllProducts/>}/>
               <Route path="/products/:keyword" element={<AllProducts/>}/>
@@ -123,10 +149,12 @@ function App() {
                   <Route path="/admin/reviews" element={<ProductReviews/>}/>
               </Route>
               <Route path="*" element={<NotFound/>}/>
-          </Routes>
+
+          </Routes></Suspense>
       </>
     </div>
   );
 }
 
 export default App;
+
