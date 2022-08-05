301c9d2b93d97009bdb191af6c488056909e5446
sourabhshukla <sourabhshukla9893502147@gmail.com>
2022-08-05 19:26:15 +0530

    Removed Admin Requirements for viewing your own order's details

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -6,7 +6,7 @@ const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 router.route("/order/new").post(isAuthenticatedUser, newOrder);
 
-router.route("/order/:id").get(isAuthenticatedUser, authorizeRoles("admin"), getSingleOrder);
+router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
 
 router.route("/orders/me").get(isAuthenticatedUser, myOrders);
 
