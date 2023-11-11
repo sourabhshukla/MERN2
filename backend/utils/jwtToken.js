68c2ac7baa2c4766aa07442df2e3eee0bb87a77f
sourabhshukla <48114727+sourabhshukla@users.noreply.github.com>
2023-11-11 23:09:56 +0530

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -4,10 +4,12 @@ const sendToken = (user, statusCode, res) => {
     //options for cookie
     const options = {
         httpOnly: true,
-        expires: new Date(
-            // Time in milliseconds
-            Date.now() + process.env.COOKIE_EXPIRE*24*60*60*1000
-        )
+        // expires: new Date(
+        //     // Time in milliseconds
+        //     Date.now() + process.env.COOKIE_EXPIRE*24*60*60*1000
+        // )
+        maxAge: 3600000,
+        secure:false
     }
 
     res.status(statusCode).cookie("token", token, options).json({
